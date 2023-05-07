754666fd493c2a7cba0fe0e0e4a0c302dd562dbe
Onyinyechi Favour <chukwumafavour600@gmail.com>
2023-05-07 03:08:33 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,8 @@
  * Return: 0 (success)
  */
 int main(void)
+
 {
-	printf("with proper grammar, the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
