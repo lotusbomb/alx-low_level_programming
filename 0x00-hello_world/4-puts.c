4f37926cb4dc260386e9f0e5094dd22abe2b4b72
Onyinyechi Favour <chukwumafavour600@gmail.com>
2023-05-07 02:21:38 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (success)
+ */
+int main(void)
+
+{
+puts("\"Programming is like building amultilingual puzzle");
+return (0);
+}
