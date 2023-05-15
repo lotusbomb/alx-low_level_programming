9bde0b9cabe045326c6a4dc49ab0f59684f8afa4
Onyinyechi Favour <chukwumafavour600@gmail.com>
2023-05-15 09:40:10 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,20 +1,21 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
-* _memset - fill a block of memory with a specific value
-* @s: starting address of memory to be filled
-* @b: the desired value
-* @n: number of bytes to be changed
-* Return: changed array with new value for n bytes
+*_memset - a function that fills memory with a constant byte
+*
+*@s: the area of memory to be filled
+*@b: the character to fill the area with
+*@n: the number of bytes to fill
+*Return: char*
 */
-
 char *_memset(char *s, char b, unsigned int n)
-
 {
-        int i = 0;
-        for (; n > 0; i++)
-        {
-                s[i] = b;
-                n--;
-        }
-        return (s);
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
